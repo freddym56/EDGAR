00abec3235b1d86ba58067dedea80bdb88a06925
lopezfr <lopezfr@sec.gov>
2026-02-18 17:32:04 -0500

    remove unnecessary import

--- validate/__init__.py
+++ validate/__init__.py
@@ -894,7 +894,7 @@ __pluginInfo__ = {
     'version': '1.26.1', # SEC EDGAR release 26.1
     'description': '''EFM Validation.''',
     'license': 'Apache-2',
-    'import': ('EDGAR/transform', 'xule', 'xule/plugin/xule'), # SEC inline can use SEC transformations
+    'import': ('EDGAR/transform', 'xule'), # SEC inline can use SEC transformations
     'author': authorLabel,
     'copyright': copyrightLabel,
     'aliases': ('validate/EFM', ),
